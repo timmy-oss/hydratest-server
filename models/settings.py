7004e312a1fbd59e22c5dba04d10c1e245977221
timmy-oss <timileyinpelumi.dev@gmail.com>
2023-01-17 19:41:49 +0100

    updated allowed origins

--- models/settings.py
+++ models/settings.py
@@ -18,16 +18,16 @@ class Settings(BaseSettings):
     debug : bool = True
     app_name : str = "HydraTest"
     ping_interval : int = 5
-    allowed_origins : list[str] = ["http://localhost:3000","https://timmy-oss-sturdy-bassoon-x47x4vqv4q536vjw-3000.preview.app.github.dev"]
+    allowed_origins : list[str] = ["http://localhost:3000","https://hydratest.vercel.app"]
     db_url : AnyUrl = "redis://127.0.0.1:10005"
     db_username : str = "default"
     db_password  : str = "root"
     jwt_secret : str = "jwt_secret"
     jwt_exp_in_mins : int = 120
     ipfs_node_url: str = 'https://ipfs.infura.io:5001'
-    infura_project_id  :str = "2CASClsLixgaD7e6qlO5LfIYA4b"
-    infura_project_secret : str = "3a6dfcb5e77b97ba69b90f55e1f7b326"
-    ipfs_read_node : str = "https://fabaaw.infura-ipfs.io/ipfs"
+    infura_project_id  :str = ""
+    infura_project_secret : str = ""
+    ipfs_read_node : str = ""
 
 
 
